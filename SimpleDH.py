b25aa30597f9a5a19ae2b0f540b71de959f61998
Kjetil Borthen <borth1@gmail.com>
2023-09-03 14:26:54 +0200

    Changed some descr. text and printout

--- SimpleDH.py
+++ SimpleDH.py
@@ -1,12 +1,13 @@
-# Simple example of Diffie Hellman principle
+# Simple example of Diffie Hellman algorithm
 
+# Encryption with XOR
 def encrypt(m, k):
-# XOR encryption
     c = ""
     for char in m:
         c = c + chr(ord(char) ^ k)
     return c
 
+# Decryption with XOR
 def decrypt(c, k):
     m = ""
     for char in c:
@@ -14,15 +15,15 @@ def decrypt(c, k):
     return m
 
 
-# Alice and Bob agree in public using these numbers
-# P: Prime
+# In clear text, Alice and Bob agree a multiplicative group of integers with
+# P: Prime, and
 # G: Generator
-# The multiplicative group of integers modulo P, where P is prime and where G is a primitve root modulo P
-# In the theory of rings, a branch of abstract algebra, it is described as the group of units of the ring of integers modulo n. Here units refers to elements with a multiplicative inverse, which, in this ring, are exactly those coprime to n.
+# where P is prime and where G is a primitve root modulo P
+# In the theory of rings, it is described as the group of units of the ring of integers modulo n. 
+# Here units refers to elements with a multiplicative inverse, which, in this ring, are exactly those coprime to n.
 P=23
 G=9
-print("Alice  --( Let use P=%i and G=%i )--> Bob" % (P,G))
-
+print("Alice  --( Msg in clear: 'Let use P=%i and G=%i' )--> Bob" % (P,G))
 
 # Alice select a random private key
 AlicePriv = 4
@@ -30,34 +31,34 @@ AlicePriv = 4
 # Bob select a random private key
 BobPriv = 3
 
-# Alice compute public value
+# Alice compute public integer
 AlicePub = (G^AlicePriv % P)
 
-# Bob compute public value
+# Bob compute public integer
 BobPub = (G^BobPriv % P)
 
-# Alice sends public value
-print("Alice  --( Here is my public integer %i )--> Bob" % AlicePub)
+# Alice sends public integer
+print("Alice  --( Msg in clear: 'Here is my public integer %i' )--> Bob" % AlicePub)
 
-# Bob sends public value
-print("Alice <--( Her is my public integer %i )--  Bob" % BobPub)
+# Bob sends public integer
+print("Alice <--( Msg in clear: 'Her is my public integer %i' )--  Bob" % BobPub)
 
-# Alice compute symmetric key
+# Alice compute symmetric secret key
 AliceSecretKey = BobPub^AlicePriv % P
 print("Alice computed secret key: %i" % AliceSecretKey)
 
-# Bob compute symmetric key
+# Bob compute symmetric secret key
 BobSecretKey = AlicePub^BobPriv % P
 print("Bob computed secret key: %i" % BobSecretKey)
 
 # Alice encrypt some text and send to Bob
 clearText = "Hi Bob!"
-print("Alice encrypt message with her secret key: %s" % clearText)
+print("Alice encrypt message with her secret key: '%s'" % clearText)
 encryptedText = encrypt(clearText, AliceSecretKey)
-print("Alice --( %s )--> Bob" % encryptedText)
+print("Alice --( Encrypted msg: '%s' )--> Bob" % encryptedText)
 
 # Bob receive encryptet text from Alice and decrypt it with his secret key
 receivedClearText = decrypt(encryptedText, BobSecretKey)
-print("Bob decrypt message from Alice with his secret key: %s" % receivedClearText)
+print("Bob decrypt message from Alice with his secret key: '%s'" % receivedClearText)
 
 
